521a894a4828e55f68ada6f9c4d01b442391568a
ThatOtherAndrew <stroev.andrew@gmail.com>
2022-12-18 01:07:55 +0000

    🪵 Add logging support in modules

--- bot/module.py
+++ bot/module.py
@@ -1,8 +1,12 @@
+import logging
+
 from discord.ext import commands
 
 from bot import Bot
 
 
 class Module(commands.Cog):
-    def __init__(self, bot: Bot):
+    def __init__(self, name: str, bot: Bot):
+        self.name = name
         self.bot = bot
+        self.logger = logging.getLogger(name)
