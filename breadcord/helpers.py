f2985b46fe788fc321f84f60e4f547630a93e8ae
ThatOtherAndrew <stroev.andrew@gmail.com>
2023-07-07 21:22:22 +0100

    📝 [Fix #85] Write docstrings for breadcord.helpers

--- breadcord/helpers.py
+++ breadcord/helpers.py
@@ -21,6 +21,24 @@ _Transformer = TypeVar('_Transformer', bound=discord.app_commands.Transformer)
 
 
 async def administrator_check(interaction: discord.Interaction) -> bool:
+    """A discord.py check to ensure that the interaction user is an administrator.
+
+    An administrator is either an owner of the bot application as seen on the Discord developer portal, or a user
+    granted administrator privileges under the ``settings.administrators`` setting.
+
+    This function should not be invoked directly, and should instead be passed to the ``discord.app_commands.check``
+    function and used as a decorator.
+
+    Example::
+
+        @app_commands.command()
+        @app_commands.check(breadcord.helpers.administrator_check)
+        async def do_admin_stuff(self, interaction: discord.Interaction, ...):
+            ...
+
+    :param interaction: The current interaction which the check should apply to.
+    """
+
     if not await interaction.client.is_owner(interaction.user):
         raise breadcord.errors.NotAdministratorError
     return True
@@ -34,6 +52,24 @@ def _search_with_key(
     threshold: float,
     max_results: int | None
 ) -> list[_T]:
+    """Internal function which implements an algorithm for fuzzy searching.
+
+    The algorithm works by assigning each string a two-part score: The partial ratio score (a metric for similarity),
+    and how far from the start of the string the optimal alignment is. Results are then sorted by highest partial ratio
+    score first, with a secondary sort of earliest alignment to ensure that matches near the start of the string are
+    shown earlier.
+
+    :param query: The string to search for.
+    :param objects: The list of strings, or other objects if using a key, to search through.
+    :param key: An optional function which is called for each object, taking it as a parameter and returning a string to
+    be used in the search process.
+    :param threshold: A number between 0 and 100 inclusive which determines the cutoff point for the similarity score.
+        A threshold of 0 means that every object will be returned in the results, whereas a threshold of 100 means that
+        only exact matches will be returned.
+    :param max_results: The maximum number of results to be returned from the search. This can be set to ``None`` to
+        return all results which pass the threshold.
+    """
+
     query = query.strip().lower()
     scored_objs: defaultdict[tuple[float, int], list[_T]] = defaultdict(list)
     strings = objects if key is None else map(key, objects)
@@ -86,6 +122,19 @@ def search_for(
     threshold: float = 80,
     max_results: int | None = 25
 ) -> list[_T]:
+    """A custom implementation of a fuzzy search algorithm.
+
+    :param query: The string to search for.
+    :param objects: The list of strings, or other objects if using a key, to search through.
+    :param key: An optional function which is called for each object, taking it as a parameter and returning a string to
+    be used in the search process.
+    :param threshold: A number between 0 and 100 inclusive which determines the cutoff point for the similarity score.
+        A threshold of 0 means that every object will be returned in the results, whereas a threshold of 100 means that
+        only exact matches will be returned.
+    :param max_results: The maximum number of results to be returned from the search. This can be set to ``None`` to
+        return all results which pass the threshold.
+    """
+
     if key is None:
         def dummy_key(item: _T) -> _T:
             return item
@@ -101,6 +150,19 @@ def simple_button(
     emoji: str | discord.Emoji | discord.PartialEmoji | None = None,
     row: int | None = None,
 ):
+    """A substitute for ``discord.ui.button`` which generates a custom ID for you.
+
+    :param label: The label of the button, if any.
+    :param style: The style of the button. Defaults to :attr:`discord.ButtonStyle.grey`.
+    :param disabled: Whether the button is disabled or not. Defaults to ``False``.
+    :param emoji: The emoji of the button. This can be in string form or a :class:`discord.PartialEmoji` or a full
+        :class:`discord.Emoji`.
+    :param row: The relative row this button belongs to. A Discord component can only have 5 rows. By default, items are
+    arranged automatically into those 5 rows. If you'd like to control the relative positioning of the row then passing
+    an index is advised. For example, row=1 will show up before row=2. Defaults to ``None``, which is automatic
+    ordering. The row number must be between 0 and 4 (i.e. zero indexed).
+    """
+
     def decorator(func: ItemCallbackType[V, Button[V]]) -> ItemCallbackType[V, Button[V]]:
         if not inspect.iscoroutinefunction(func):
             raise TypeError('button function must be a coroutine function')
@@ -127,6 +189,39 @@ def simple_button(
 
 
 def simple_transformer(to: type[_T]) -> Callable[[type[_Transformer]], _Transformer]:
+    """A decorator for discord.py transformers which makes them easier to use in type annotations.
+
+    Before::
+
+        class BooleanTransformer(app_commands.Transformer):
+            def transform(self, interaction: discord.Interaction, value: str) -> bool:
+                return True if value.strip().lower() in ('true', 'yes', 'y') else False
+
+        @app_commands.command()
+        async def say_hello(
+            interaction: discord.Interaction,
+            all_caps: app_commands.Transform[bool, BooleanTransforer]
+        ):
+            await interaction.response.send_message('HELLO' if all_caps else 'hello')
+
+        transformer = BooleanTransformer()
+        manual_transform = transformer.transform(some_interaction, 'yes')
+
+    After::
+
+        @breadcord.helpers.simple_transformer(bool)
+        class BooleanTransformer(app_commands.Transformer):
+            def transform(self, interaction: discord.Interaction, value: str) -> bool:
+                return True if value.strip().lower() in ('true', 'yes', 'y') else False
+
+        @app_commands.command()
+        async def say_hello(interaction: discord.Interaction, all_caps: BooleanTransformer):
+            await interaction.response.send_message('HELLO' if all_caps else 'hello')
+
+        manual_transform = BooleanTransformer.transform(some_interaction, 'yes')
+
+    :param to: The type which the transformer should output.
+    """
     def decorator(cls: type[_Transformer]) -> _Transformer:
         return discord.app_commands.Transform.__class_getitem__((to, cls))
 
