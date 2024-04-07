b75cc57532ba9828fe98fbb465824ab452cd04b4
Fripe <72686066+Fripe070@users.noreply.github.com>
2024-04-07 18:59:07 +0200

    Improvements to auto update core module (#218)

--- breadcord/core_modules/auto_update/__init__.py
+++ breadcord/core_modules/auto_update/__init__.py
@@ -6,6 +6,7 @@ import subprocess
 from functools import cache
 from typing import Any
 
+import discord
 from discord.ext import commands, tasks
 
 import breadcord
@@ -47,7 +48,7 @@ async def git(*command_arguments: str, timeout: float = 10, **kwargs: Any) -> st
 class AutoUpdate(breadcord.module.ModuleCog):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        git_path()  # Error early if not git path is found
+        git_path()  # Error early if no git path is found
         self.loop = None
 
     async def cog_load(self) -> None:
@@ -55,19 +56,24 @@ class AutoUpdate(breadcord.module.ModuleCog):
         def on_update_interval_changed(_, new: float) -> None:
             if self.loop is not None:
                 self.loop.stop()
-            # Are you kidding me? SQL injection from Git?
-            self.logger.debug(f'Auto update interval now set to {new}')  # noqa: S608
-            self.loop = tasks.loop(hours=new)(self.update_modules)
+            # Are you kidding me? SQL injection in logger.debug?
+            self.logger.debug(f'Auto update interval set to {new} minutes')  # noqa: S608
+            self.loop = tasks.loop(minutes=new)(self.update_modules)
             self.loop.start()
 
-        on_update_interval_changed(0, self.settings.update_interval.value)
+        async def wait_for_ready():
+            while not self.bot.ready:
+                await asyncio.sleep(1)
+            on_update_interval_changed(0, self.settings.update_interval.value)
+        task = asyncio.create_task(wait_for_ready())
+        task.add_done_callback(lambda _: self.logger.debug('Auto update task scheduled and ran'))
 
-    async def update_modules(self) -> None:
-        if self.loop.current_loop == 0:
-            return
-
+    async def update_modules(self) -> dict[str, tuple[str, str, str]]:
+        updated_modules = {}
         self.logger.info('Attempting to update modules')
         for module in self.bot.modules:
+            if not module.loaded:
+                continue
             if not (module.path / '.git' / 'HEAD').is_file():
                 continue
             try:
@@ -78,27 +84,64 @@ class AutoUpdate(breadcord.module.ModuleCog):
                     await git('rev-parse', 'HEAD', cwd=module.path)
                 ):
                     self.logger.debug(f'Module {module.id} is up-to-date.')
-                    return
+                    continue
                 self.logger.warning(f'Module {module.id} is out-of-date.')
-                await self.update_module(module)
+                pull_msg, commit_hash, commit_msg = await self.update_module(module)
+                updated_modules[module.id] = pull_msg, commit_hash, commit_msg
             except subprocess.CalledProcessError as error:
                 self.logger.error(f'Failed to fetch updates for the module {module.id!r}: {error}\n{error.stderr}')
 
-    async def update_module(self, module: breadcord.module.Module) -> None:
+        if updated_modules:
+            self.logger.info(f'Finished updating len({updated_modules}) modules')
+        else:
+            self.logger.debug('No modules were updated')
+        return updated_modules
+
+    async def update_module(self, module: breadcord.module.Module) -> tuple[str, str, str]:
         self.logger.info(f'Updating {module.id}')
         update_text = await git('pull', cwd=module.path)
         self.logger.debug(f'({module.id}) Git output:\n{update_text.strip()}')
         await module.reload()
 
-        git_hash_msg = await git('log', '-1', '--format="%H %s"', cwd=module.path)
-        self.logger.debug(f'Module {module.id} now on: {git_hash_msg[1:-2]}')
+        git_hash_msg = (await git('log', '-1', '--format="%H %s"', cwd=module.path)).strip().strip('"')
+        self.logger.debug(f'Updated {module.id} to {git_hash_msg}')
+
+        parts = git_hash_msg.strip().split(' ', 1)
+        return update_text, parts[0], ' '.join(parts[1:])
 
     @commands.command()
     @commands.is_owner()
     async def update(self, ctx: commands.Context) -> None:
         message = await ctx.send('Updating...')
-        await self.update_modules()
-        await message.edit(content='Finished updating modules.')
+        updated = await self.update_modules()
+        if len(updated) == 0:
+            await message.edit(content='No modules were updated.')
+            return
+
+        embeds = [
+            discord.Embed(
+                title=f'Updated module `{module_id}`',
+                description='\n'.join(filter(bool, (
+                    f'**Latest commit message**: {discord.utils.escape_markdown(commit_msg)}',
+                    f'```\n{pull_msg}\n```' if pull_msg else None,
+                ))),
+                color=discord.Colour.green(),
+            ).set_footer(text=f'Now on commit {commit_hash}')
+            for module_id, (pull_msg, commit_hash, commit_msg) in updated.items()
+        ]
+
+        max_embeds = 10
+        if len(embeds) > max_embeds:
+            embeds = embeds[:max_embeds-1]
+            embeds.append(discord.Embed(
+                title='And more...',
+                description=f'{len(updated) - len(embeds)} more modules were updated.',
+                color=discord.Colour.orange(),
+            ))
+        await message.edit(
+            content='Finished updating modules.',
+            embeds=embeds,
+        )
 
 
 async def setup(bot: breadcord.Bot):
