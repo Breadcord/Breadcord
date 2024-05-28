f356c6e4bb8cc41118a029da9f26acba1234a85b
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2024-05-28 20:41:24 +0200

    replace lovecraftian horror with regex

--- breadcord/helpers.py
+++ breadcord/helpers.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import inspect
 import logging
+import re
 import sys
 from collections import defaultdict
 from typing import TYPE_CHECKING, Self, TypeVar, overload
@@ -278,25 +279,6 @@ class IndentFormatter(logging.Formatter):
         super().__init__()
         self._wrapped = to_wrap or logging.Formatter()
 
-    @staticmethod
-    def _strip_colour_ansi(string: str) -> str:
-        """Strips ANSI colour codes from a string."""
-        # If this does not match everything we want it to, I take no responsibility - Fripe
-        new = ''
-        i = 0
-        while i < len(string):
-            if string[i] == '\x1b' and string[i + 1] == '[':
-                end = string.find('m', i+2)
-                if end == -1:
-                    new += string[i:]
-                    break
-                if all(char in '0123456789;' for char in string[i+2:end]):
-                    i = end + 1
-                    continue
-            new += string[i]
-            i += 1
-        return new
-
     def get_prefix_length(self, record: logging.LogRecord) -> int:
         """Get the length of the prefix of the log message. Will not give wanted results if ANSI is involved."""
         formatted = self._wrapped.format(logging.LogRecord(
@@ -309,7 +291,7 @@ class IndentFormatter(logging.Formatter):
             exc_info=None,
         ))
         formatted = formatted.splitlines()[-1]  # Might as well anticipate something scuffed
-        formatted = self._strip_colour_ansi(formatted)
+        formatted = re.sub(r'\x1b\[[0-9;]*m', '', formatted)
         return len(formatted)
 
     def format(self, record: logging.LogRecord) -> str:
